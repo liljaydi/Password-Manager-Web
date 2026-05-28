ba4123207cf2b8437f0f8c4543ac2b0dfce7bffa
liljaydi <gydetaparan@gmail.com>
2026-05-28 12:23:48 +0800

    added highlight when clicked (not being checked)

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const checkboxes = document.querySelectorAll('.checkbox')
+const rows = document.querySelectorAll('.account-row')
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener('click', () => {
-        checkbox.parentElement.classList.toggle('selected')
+        checkbox.parentElement.classList.toggle('checked-selected')
+    })
+})
+
+rows.forEach(row => {
+    row.addEventListener('click', () => {
+        rows.forEach(row => {
+            row.classList.remove('selected')
+        })
+        row.classList.toggle('selected')
     })
 })
